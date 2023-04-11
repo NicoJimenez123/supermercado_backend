31aa80d80e7622e3c5ea27af147dbf0d589aa77d
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2023-04-10 21:40:50 -0300

    Corregidos imports sin usar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 
-import { Router } from 'express'
-import res from 'express/lib/response'
-
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send("Hola Mundo")
+  res.send("Hola Mundo desde Express")
 })
 
-app.listen(3000, () => console.log("Andando en el puerto 3000"))
+app.listen(3030, () => console.log("Andando en el puerto 3000"))
