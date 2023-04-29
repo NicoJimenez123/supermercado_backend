23e0501b36082e84881a9bdbbcddf2ca9e15d661
Leonardo <82182374+Leo004f@users.noreply.github.com>
2023-04-29 14:32:57 -0300

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send("Hola Mundo desde Express")
-})
-
-app.listen(3000, () => console.log("Andando en el puerto 3000"))
