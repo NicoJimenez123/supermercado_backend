e3818527391564c665dc6df8ed6145795fd0f665
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-01 21:36:48 -0300

    Añadida lógica para iniciar el servidor

--- src/app.js
+++ src/app.js
@@ -8,5 +8,7 @@ const productoRuta = require('../routes/ProductosRouter')
 
 app.use('/tienda/productos', productoRuta);
 
-
-module.exports = app;
+const puerto = process.env.port || 3000
+app.listen(puerto, () => {
+  console.log("Servidor corriendo en el puerto: " + puerto)
+})
